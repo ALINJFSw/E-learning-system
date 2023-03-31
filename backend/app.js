9d149adc98c23e83fc124d4e3872a9022b2abea8
AlINJFSw <alijanbeincs@gmail.com>
2023-04-01 02:32:10 +0300

    added handling error

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,19 @@ app.use(express.json())
 const mongoose = require("mongoose");
 require('dotenv').config()
 const userRoute = require("./router/auth.route");
+const HttpError = require("./support/http-error")
 
 app.use("/auth",userRoute);
+
+app.use((req, res, next) => {
+    const error = new HttpError("can't find route",404);
+    return next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.code || 400);
+    res.send({ message: error.message || "sonthing went wrong !" });
+  });
 
 app.listen(process.env.PORT,()=> {
     console.log("server is runnig now")
